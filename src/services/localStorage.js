f17e055be19efb750e076a036e7b1ad1baaecb1d
YeissonOrtiz <ortizyeison2183@gmail.com>
2023-09-10 17:28:16 -0500

    change structure localStorage hook

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -2,40 +2,26 @@
 
 import { useState, useEffect } from "react";
 
-
 const useLocalStorage = (key, defaultValue) => {
-	useEffect(() => {
-		const localStorage = window ? window?.localStorage : null
-		const [localStorageValue, setLocalStorageValue] = useState(() => {
-			try {
-				const value = localStorage.getItem(key)
-				
-				if (value) {
-					return JSON.parse(value)
-				} else {
-					localStorage.setItem(key, JSON.stringify(defaultValue));
-					return defaultValue
-				}
-			} catch (error) {
-				localStorage.setItem(key, JSON.stringify(defaultValue));
-				return defaultValue
-			}
-		})
-	
-		const setLocalStorageStateValue = (valueOrFn) => {
-			let newValue;
-			if (typeof valueOrFn === 'function') {
-				const fn = valueOrFn;
-				newValue = fn(localStorageValue)
-			}
-			else {
-				newValue = valueOrFn;
-			}
-			localStorage.setItem(key, JSON.stringify(newValue));
-			setLocalStorageValue(newValue)
-		}
-		return [localStorageValue, setLocalStorageStateValue]
-	}, [])
-}
+  useEffect(() => {
+    const localStorageValue = JSON.parse(localStorage.getItem(key)) || defaultValue;
+
+    const setLocalStorageStateValue = (valueOrFn) => {
+      let newValue;
+      if (typeof valueOrFn === 'function') {
+        const fn = valueOrFn;
+        newValue = fn(localStorageValue);
+      } else {
+        newValue = valueOrFn;
+      }
+      localStorage.setItem(key, JSON.stringify(newValue));
+    };
+
+    setLocalStorageStateValue(defaultValue);
+  }, [key, defaultValue]); // Include key and defaultValue in the dependency array
+
+  // Return the localStorage value and setter function
+  return [localStorageValue, setLocalStorageStateValue];
+};
 
 export { useLocalStorage };
